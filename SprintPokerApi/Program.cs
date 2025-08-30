3853913aae1a2537239fb85c2436a0744c2d345a
Jason Hamilton <musicman3569@gmail.com>
2025-08-30 13:53:09 -0600

    Add Npgsql to Program.cs

--- SprintPokerApi/Program.cs
+++ SprintPokerApi/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using SprintPokerApi.Data;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development";
+
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console() // STDOUT
+    .CreateLogger();
 
 // Add services to the container.
 
@@ -6,6 +17,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure Entity Framework Core with PostgreSQL
+// Ensure you have the Npgsql.EntityFrameworkCore.PostgreSQL package installed
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString, npgsql =>
+        npgsql.MigrationsHistoryTable("__EFMigrationsHistory", "sprintpoker")
+    )
+);
 
 var app = builder.Build();
 
