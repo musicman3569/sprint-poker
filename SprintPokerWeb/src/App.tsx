426d95210be1b257122317f255182d02e96fd785
Jason Hamilton <musicman3569@gmail.com>
2025-08-31 12:05:22 -0600

    Add Keycloak auth to React frontend

--- SprintPokerWeb/src/App.tsx
+++ SprintPokerWeb/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { PrimeReactProvider } from 'primereact/api';
+import 'primereact/resources/themes/soho-dark/theme.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
 import './App.css'
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloak from "./utils/KeycloakInit"
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <ReactKeycloakProvider
+          authClient={keycloak}
+          initOptions={{
+              onLoad: 'login-required',
+              pkceMethod: 'S256',
+              checkLoginIframe: false,
+          }}
+      >
+          <PrimeReactProvider>
+              <div className="App">
+                  <h1>Sprint Poker</h1>
+              </div>
+          </PrimeReactProvider>
+      </ReactKeycloakProvider>
+  );
 }
 
 export default App
